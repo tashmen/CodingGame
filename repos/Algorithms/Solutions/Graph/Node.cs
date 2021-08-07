49790b826b3d3e5da2a8d32ed324889be4ab90fc
tashman <tash_men@yahoo.com>
2021-08-07 16:08:06 -0400

    Add algorithms project for consolidating frequently used code that will be the backbone to solving multiple puzzles.

---
+++ repos/Algorithms/Solutions/Graph/Node.cs
@@ -0,0 +1,23 @@
+﻿namespace Algorithms.Graph
+{
+    public class Node
+    {
+        public int Id { get; set; }
+        public int Distance { get; set; }
+        public Node(int id, int distance)
+        {
+            Id = id;
+            Distance = distance;
+        }
+
+        /// <summary>
+        /// Creates a clone of the node from the current distance.  This is used while building the minimum spanning tree.
+        /// </summary>
+        /// <param name="currentDist">The current distance from the starting node</param>
+        /// <returns>A clone of the node with the proper distance</returns>
+        public Node CreateAtDistance(int currentDist)
+        {
+            return new Node(Id, currentDist + Distance);
+        }
+    }
+}
---
+++ repos/Algorithms/UnitTest/Graph/GraphLinksTests.cs
@@ -0,0 +1,38 @@
+﻿using Algorithms.Graph;
+using Xunit;
+
+namespace UnitTest.Utilities
+{
+    public class GraphLinksTests
+    {
+        GraphLinks _links;
+        public GraphLinksTests()
+        {
+            _links = new GraphLinks();
+            _links.AddLink(1, 2, 1);
+            _links.AddLink(1, 3, 2);
+            _links.AddLink(1, 4, 4);
+            _links.AddLink(2, 3, 1);
+            _links.AddLink(2, 4, 2);
+
+            _links.CalculateShortestPaths();
+        }
+
+        [Fact]
+        public void Test_ShortestPath()
+        {
+            Assert.Equal(3, _links.GetShortestPath(1, 3));
+            Assert.Equal(2, _links.GetShortestPath(1, 4));
+
+            Assert.Equal(2, _links.GetShortestPathDistance(1, 3));
+            Assert.Equal(3, _links.GetShortestPathDistance(1, 4));
+        }
+
+        [Fact]
+        public void Test_Distance()
+        {
+            Assert.Equal(2, _links.GetDistance(1, 3));
+            Assert.Equal(4, _links.GetDistance(1, 4));
+        }
+    }
+}
