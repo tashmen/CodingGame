c097dcc59ab1cd328523f1f571187670cfa45330
tashman <tash_men@yahoo.com>
2021-08-08 17:31:57 -0400

    Move the node implementation into the graph links to keep it separate.

--- repos/Algorithms/Solutions/Graph/GraphLinks.cs
+++ repos/Algorithms/Solutions/Graph/GraphLinks.cs
@@ -6,6 +6,31 @@ namespace Algorithms.Graph
 {
     public class GraphLinks
     {
+        public class Node
+        {
+            public int Id { get; set; }
+            public double Distance { get; set; }
+
+            public bool IsExplored { get; set; }
+            public Node(int id, double distance)
+            {
+                Id = id;
+                Distance = distance;
+            }
+
+
+
+            /// <summary>
+            /// Creates a clone of the node from the current distance.  This is used while building the minimum spanning tree.
+            /// </summary>
+            /// <param name="currentDist">The current distance from the starting node</param>
+            /// <returns>A clone of the node with the proper distance</returns>
+            public Node CreateAtDistance(double currentDist)
+            {
+                return new Node(Id, currentDist + Distance);
+            }
+        }
+
         private Dictionary<int, List<Node>> Links { get; set; }
         private Dictionary<int, Dictionary<int, List<Node>>> Paths { get; set; }
         private bool IsByDirectional;
