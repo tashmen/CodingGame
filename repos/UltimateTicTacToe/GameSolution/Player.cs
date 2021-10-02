82a52096f9ad81ebf8e473a095825c827e0b86b9
tashman <tash_men@yahoo.com>
2021-10-02 10:33:20 -0400

    Adding base project for Ultimate Tic Tac Toe

---
+++ repos/UltimateTicTacToe/GameSolution/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs;
+
+        // game loop
+        while (true)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int opponentRow = int.Parse(inputs[0]);
+            int opponentCol = int.Parse(inputs[1]);
+            int validActionCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < validActionCount; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int row = int.Parse(inputs[0]);
+                int col = int.Parse(inputs[1]);
+            }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine("0 0");
+        }
+    }
+}
