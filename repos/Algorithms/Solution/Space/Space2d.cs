e66594a612f3cf41771aab37acc7dbfafbae911f
tashman <tash_men@yahoo.com>
2022-04-28 11:20:31 -0400

    Fix namespaces

--- repos/Algorithms/Solution/Space/Space2d.cs
+++ repos/Algorithms/Solution/Space/Space2d.cs
@@ -1,9 +1,7 @@
-﻿using Solution.Space;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using System.Text;
 
 namespace Algorithms.Space
 {
