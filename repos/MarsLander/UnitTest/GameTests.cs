6a9f7c449efbb0d2689fb5aef08078716d8fe0cf
tashman <tash_men@yahoo.com>
2022-04-29 21:13:32 -0400

    Add Mars Lander project

---
+++ repos/MarsLander/UnitTest/GameTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit.Abstractions;
+
+namespace UnitTest
+{
+
+
+
+    public class GameTests
+    {
+        private Converter converter;
+
+        public GameTests(ITestOutputHelper output)
+        {
+            converter = new Converter(output);
+            Console.SetError(converter);
+
+        }
+
+      
+    }
+}
