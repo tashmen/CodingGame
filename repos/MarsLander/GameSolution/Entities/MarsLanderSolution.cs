a2fc8049ae0084cad221324c4ef4d8ddad0a30dc
tashman <tash_men@yahoo.com>
2022-05-21 19:00:55 -0400

    Move Gamehelper to the unit tests Remove the unused Draw project The test cases needed a few extra turns so added 5 more

--- repos/MarsLander/GameSolution/Entities/MarsLanderSolution.cs
+++ repos/MarsLander/GameSolution/Entities/MarsLanderSolution.cs
@@ -9,7 +9,7 @@ namespace GameSolution.Entities
     {
         public StaticMove[] Moves;
         private Random Rand;
-        private static int TotalMoves = 155;
+        private static int TotalMoves = 160;
         public double Fitness { get; set; }
         public GameState State;
         public int Turn = 0;
